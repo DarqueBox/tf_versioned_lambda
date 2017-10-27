d3d49683a97e37c10966fc43c0932759943e461b
Davis McClellan <dmcclellan@instructure.com>
2017-10-26 19:40:58 -0600

    Add vpc config variables

--- modules/node/variables.tf
+++ modules/node/variables.tf
@@ -47,7 +47,6 @@ variable "timeout" {
   default     = "3"
 }
 
-/* Not Working ATM
 variable "vpc_subnet_ids" {
   description = "pass subnets to run inside a VPC, must be used with vpc_security_group_ids"
   default     = []
@@ -57,7 +56,6 @@ variable "vpc_security_group_ids" {
   description = "pass security group ids to run inside a VPC, must be used with vpc_subnet_ids"
   default     = []
 }
-*/
 
 variable "build_script" {
   description = "allow for passing a custom build script, which overrides default build_docker.sh"
